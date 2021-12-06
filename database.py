5e9a19b2cc3fcd80fae86261afd7ef36b3f1347a
1nFernous <mcglowstring@gmail.com>
2021-12-06 22:43:35 +0700

    PISAH

--- database.py
+++ database.py
@@ -13,14 +13,19 @@ conn = sqlite3.connect('course.db')
 #membuat cursor untuk membuat query
 c = conn.cursor()
 '''
-c.execute("CREATE TABLE course(id INTEGER PRIMARY KEY, name TEXT, sks INTEGER, term INTEGER)")
-c.execute("CREATE TABLE harienum(id INTEGER PRIMARY KEY AUTOINCREMENT, hari TEXT)")
-c.execute("INSERT INTO harienum(hari) VALUES ('SENIN'), ('SELASA'), ('RABU'), ('KAMIS'), ('JUMAT'), ('SABTU'), ('MINGGU')")
-c.execute("CREATE TABLE pertemuan(id INTEGER PRIMARY KEY, course_id REFERENCES course(id), hari REFERENCES harienum(id), jam_start TEXT, jam_end TEXT)")
-c.execute("INSERT INTO course VALUES(1, 'Despro', 3, 7), (2, 'Sistem Waktu Nyata dan IoT', 3, 5), (3, 'Teknologi Big Data', 3, 7), (4, 'Jaringan Komputer', 4, 3), (5, 'Keamanan Jaringan', 3, 5)")
-c.execute("INSERT INTO pertemuan VALUES(1, 1, 5, '2021-10-13 16:00:00.000', '2021-10-13 18:30:00.000'), (2, 2, 4, '2021-10-13 16:00:00.000', '2021-10-13 18:30:00.000'), (3, 3, 1, '2021-10-13 16:00:00.000', '2021-10-13 18:30:00.000'), (4, 4, 2, '2021-10-13 08:00:00.000', '2021-10-13 09:40:00.000'), (5, 4, 4, '2021-10-13 08:00:00.000', '2021-10-13 09:40:00.000'),(6, 5, 3, '2021-10-13 16:00:00.000', '2021-10-13 18:30:00.000')")
-c.execute("CREATE TABLE mycourse(id INTEGER PRIMARY KEY AUTOINCREMENT, course_id REFERENCES course(id))")
+c.execute("DROP TABLE mycourse")
+c.execute("DROP TABLE harienum")
+c.execute("DROP TABLE pertemuan")
+c.execute("DROP TABLE course")
 '''
+c.execute("CREATE TABLE harienum(id INTEGER PRIMARY KEY AUTOINCREMENT, hari TEXT)")
+c.execute("CREATE TABLE pertemuan(id INTEGER PRIMARY KEY AUTOINCREMENT, hari REFERENCES harienum(id), jam_start TEXT, jam_end TEXT)")
+c.execute("CREATE TABLE course(id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, sks INTEGER, term INTEGER, pertemuan_id REFERENCES pertemuan(id))")
+c.execute("CREATE TABLE mycourse(id INTEGER PRIMARY KEY AUTOINCREMENT, course_id REFERENCES course(id))")
+c.execute("INSERT INTO harienum(hari) VALUES ('SENIN'), ('SELASA'), ('RABU'), ('KAMIS'), ('JUMAT'), ('SABTU'), ('MINGGU')")
+c.execute("INSERT INTO pertemuan(hari, jam_start, jam_end) VALUES (1, '2021-10-13 08:00:00.000', '2021-10-13 09:40:00.000'), (1, '2021-10-13 10:00:00.000', '2021-10-13 11:40:00.000'),(1, '2021-10-13 13:00:00.000', '2021-10-13 14:40:00.000'), (1, '2021-10-13 16:00:00.000', '2021-10-13 17:40:00.000'), (1, '2021-10-13 08:00:00.000', '2021-10-13 10:30:00.000'), (1, '2021-10-13 10:00:00.000', '2021-10-13 12:30:00.000'),(1, '2021-10-13 13:00:00.000', '2021-10-13 15:30:00.000'), (1, '2021-10-13 16:00:00.000', '2021-10-13 18:30:00.000'), (2, '2021-10-13 08:00:00.000', '2021-10-13 09:40:00.000'), (2, '2021-10-13 10:00:00.000', '2021-10-13 11:40:00.000'),(2, '2021-10-13 13:00:00.000', '2021-10-13 14:40:00.000'), (2, '2021-10-13 16:00:00.000', '2021-10-13 17:40:00.000'), (2, '2021-10-13 08:00:00.000', '2021-10-13 10:30:00.000'), (2, '2021-10-13 10:00:00.000', '2021-10-13 12:30:00.000'),(2, '2021-10-13 13:00:00.000', '2021-10-13 15:30:00.000'), (2, '2021-10-13 16:00:00.000', '2021-10-13 18:30:00.000'), (3, '2021-10-13 08:00:00.000', '2021-10-13 09:40:00.000'), (3, '2021-10-13 10:00:00.000', '2021-10-13 11:40:00.000'),(3, '2021-10-13 13:00:00.000', '2021-10-13 14:40:00.000'), (3, '2021-10-13 16:00:00.000', '2021-10-13 17:40:00.000'), (3, '2021-10-13 08:00:00.000', '2021-10-13 10:30:00.000'), (3, '2021-10-13 10:00:00.000', '2021-10-13 12:30:00.000'),(3, '2021-10-13 13:00:00.000', '2021-10-13 15:30:00.000'), (3, '2021-10-13 16:00:00.000', '2021-10-13 18:30:00.000'), (4, '2021-10-13 08:00:00.000', '2021-10-13 09:40:00.000'), (4, '2021-10-13 10:00:00.000', '2021-10-13 11:40:00.000'),(4, '2021-10-13 13:00:00.000', '2021-10-13 14:40:00.000'), (4, '2021-10-13 16:00:00.000', '2021-10-13 17:40:00.000'), (4, '2021-10-13 08:00:00.000', '2021-10-13 10:30:00.000'), (4, '2021-10-13 10:00:00.000', '2021-10-13 12:30:00.000'),(4, '2021-10-13 13:00:00.000', '2021-10-13 15:30:00.000'), (4, '2021-10-13 16:00:00.000', '2021-10-13 18:30:00.000'), (5, '2021-10-13 08:00:00.000', '2021-10-13 09:40:00.000'), (5, '2021-10-13 10:00:00.000', '2021-10-13 11:40:00.000'),(5, '2021-10-13 13:00:00.000', '2021-10-13 14:40:00.000'), (5, '2021-10-13 16:00:00.000', '2021-10-13 17:40:00.000'), (5, '2021-10-13 08:00:00.000', '2021-10-13 10:30:00.000'), (5, '2021-10-13 10:00:00.000', '2021-10-13 12:30:00.000'),(5, '2021-10-13 13:00:00.000', '2021-10-13 15:30:00.000'), (5, '2021-10-13 16:00:00.000', '2021-10-13 18:30:00.000')")
+c.execute("INSERT INTO course(name, sks, term, pertemuan_id) VALUES ('Despro', 3, 7, 40),('Sistem Waktu Nyata dan IoT', 3, 5, 32),('Teknologi Big Data', 3, 7 , 8),('Jaringan Komputer Selasa', 2, 5, 9),('Jaringan Komputer Kamis', 2, 5, 25),('Dasar Sitem Digital',3, 1, 38),('Jaringan Telekomunikasi',3, 5, 13),('Object Oriented Programming',3, 3, 23),('Probstok',2, 5, 12),('RPL',3, 5, 7),('Statistika',3, 3, 37),('Diskrit',3, 3, 26),('Sinsis',2, 5, 25),('Rangkaian Listrik',2, 3, 11)")
+
 
 c.execute("SELECT * FROM pertemuan")
 #c.execute("DROP TABLE mycourse")
--- main.go
+++ main.go
@@ -6,55 +6,53 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
 	"github.com/gorilla/mux"
 	_ "github.com/mattn/go-sqlite3"
 )
 
 type Course struct {
-	Course_id int `json: "course_id"`
-	Name string `json: "name"`
-	Sks int `json: "sks"`
-	Term int `json: "term"`
+	Course_id int    `json: "course_id"`
+	Name      string `json: "name"`
+	Sks       int    `json: "sks"`
+	Term      int    `json: "term"`
 	Jam_start string `json: "jam_start"`
-	Jam_end string `json: "jam_end"`
-	Hari string `json: "hari"`
+	Jam_end   string `json: "jam_end"`
+	Hari      string `json: "hari"`
 }
 
 type MyCourse struct {
-	Mycourse_id int `json: "mycourse_id"`
-	Course_id int `json: "course_id"`
-	Name string `json: "name"`
-	Sks int `json: "sks"`
-	Term int `json: "term"`
-	Jam_start string `json: "jam_start"`
-	Jam_end string `json: "jam_end"`
-	Hari string `json: "hari"`
+	Mycourse_id int    `json: "mycourse_id"`
+	Course_id   int    `json: "course_id"`
+	Name        string `json: "name"`
+	Sks         int    `json: "sks"`
+	Term        int    `json: "term"`
+	Jam_start   string `json: "jam_start"`
+	Jam_end     string `json: "jam_end"`
+	Hari        string `json: "hari"`
 }
 
-func main()  {
-	
+func main() {
 	handleRequests()
 }
-
-
 
 func home(w http.ResponseWriter, r *http.Request) {
 
 	db, err := sql.Open("sqlite3", "./course.db")
-	
+
 	if err != nil {
 		log.Fatal(err)
 	}
-	
+
 	//rows, err := db.Query("SELECT * FROM pertemuan")
-	rows, err := db.Query("SELECT course.id AS course_id, course.name AS name, course.sks AS sks, course.term AS term, time(pertemuan.jam_start) AS jam_start, time(pertemuan.jam_end) AS jam_end, harienum.hari AS hari FROM course INNER JOIN pertemuan ON pertemuan.course_id = course.id INNER JOIN harienum ON harienum.id = pertemuan.hari GROUP BY harienum.hari ORDER BY term")
+	rows, err := db.Query("SELECT course.id AS course_id, course.name AS name, course.sks AS sks, course.term AS term, time(pertemuan.jam_start) AS jam_start, time(pertemuan.jam_end) AS jam_end, harienum.hari AS hari FROM course INNER JOIN pertemuan ON course.pertemuan_id = pertemuan.id INNER JOIN harienum ON harienum.id = pertemuan.hari ORDER BY term")
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	Courses := []Course{}
 	for rows.Next() {
-		
+
 		var c Course
 		err = rows.Scan(&c.Course_id, &c.Name, &c.Sks, &c.Term, &c.Jam_start, &c.Jam_end, &c.Hari)
 		if err != nil {
@@ -63,19 +61,17 @@ func home(w http.ResponseWriter, r *http.Request) {
 		Courses = append(Courses, c)
 	}
 
-
 	//fmt.Fprintf(w, Courses)
 	fmt.Println("Home")
-	
+
 	json.NewEncoder(w).Encode(Courses)
-	
-	
+
 }
 
 // Nambah matkul
 func addCourse(w http.ResponseWriter, r *http.Request) {
 	decoder := json.NewDecoder(r.Body)
-	type test_struct struct{
+	type test_struct struct {
 		Name string
 	}
 	var name test_struct
@@ -85,26 +81,76 @@ func addCourse(w http.ResponseWriter, r *http.Request) {
 	}
 
 	db, err := sql.Open("sqlite3", "./course.db")
-	
+
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	var id int 
+	var id int
 	test, err := db.Query("SELECT id FROM course WHERE name=$1", name.Name)
 
 	if err != nil {
 		log.Fatal(err)
 	}
-	
-	
+
 	for test.Next() {
 		err = test.Scan(&id)
 		if err != nil {
 			log.Fatal(err)
 		}
 		fmt.Println(id)
-	} 
+	}
+
+	var hari string
+	var jam string
+	var hariada string
+	var jamada string
+
+	cekhari, err := db.Query("SELECT harienum.hari FROM course INNER JOIN pertemuan ON course.pertemuan_id = pertemuan.id INNER JOIN harienum ON harienum.id = pertemuan.hari WHERE course.id=$1", id)
+	if err != nil {
+		log.Fatal(err)
+	}
+	cekjam, err := db.Query("SELECT pertemuan.jam_start FROM course INNER JOIN pertemuan ON course.pertemuan_id = pertemuan.id INNER JOIN harienum ON harienum.id = pertemuan.hari WHERE course.id=$1", id)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for cekhari.Next() {
+		if err := cekhari.Scan(&hari); err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	for cekjam.Next() {
+		if err := cekjam.Scan(&jam); err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	adahari, err := db.Query("SELECT harienum.hari FROM mycourse INNER JOIN course ON course.id = mycourse.course_id INNER JOIN pertemuan ON pertemuan.id = course.pertemuan_id INNER JOIN harienum ON harienum.id = pertemuan.hari WHERE harienum.hari=$1", hari)
+	if err != nil {
+		log.Fatal(err)
+	}
+	adajam, err := db.Query("SELECT pertemuan.jam_start FROM mycourse INNER JOIN course ON course.id = mycourse.course_id INNER JOIN pertemuan ON pertemuan.id = course.pertemuan_id INNER JOIN harienum ON harienum.id = pertemuan.hari WHERE pertemuan.jam_start=$1", jam)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for adahari.Next() {
+		if err := adahari.Scan(&hariada); err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	for adajam.Next() {
+		if err := adajam.Scan(&jamada); err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	if hari == hariada && jam == jamada {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("500 - Something bad happened!"))
+	}
 
 	_, err = db.Exec("INSERT INTO mycourse(course_id) VALUES($1)", id)
 	//_, err = db.Exec("INSERT INTO mycourse(course_id) VALUES(1)")
@@ -112,29 +158,24 @@ func addCourse(w http.ResponseWriter, r *http.Request) {
 		log.Fatal(err)
 	}
 
-	
-
 }
 
-
-
-
-func getMyCourse(w http.ResponseWriter, r *http.Request){
+func getMyCourse(w http.ResponseWriter, r *http.Request) {
 	db, err := sql.Open("sqlite3", "./course.db")
-	
+
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	rows, err := db.Query("SELECT mycourse.id AS mycourse_id, mycourse.course_id AS course_id, course.name AS name, course.sks AS sks, course.term AS term, pertemuan.jam_start AS jam_start, pertemuan.jam_end AS jam_end, harienum.hari AS hari FROM mycourse INNER JOIN course ON course.id = mycourse.course_id INNER JOIN pertemuan ON pertemuan.course_id = course.id INNER JOIN harienum ON harienum.id = pertemuan.hari ORDER BY term")
+	rows, err := db.Query("SELECT mycourse.id, mycourse.course_id AS course_id, course.name AS name, course.sks AS sks, course.term AS term, time(pertemuan.jam_start) AS jam_start, time(pertemuan.jam_end) AS jam_end, harienum.hari AS hari FROM mycourse INNER JOIN course ON course.id = mycourse.course_id INNER JOIN pertemuan ON pertemuan.id = course.pertemuan_id INNER JOIN harienum ON harienum.id = pertemuan.hari ORDER BY term")
 
 	if err != nil {
 		log.Fatal(err)
-	}	
+	}
 
 	Mycourse := []MyCourse{}
 
-	for rows.Next(){
+	for rows.Next() {
 		var mc MyCourse
 		err := rows.Scan(&mc.Mycourse_id, &mc.Course_id, &mc.Name, &mc.Sks, &mc.Term, &mc.Jam_start, &mc.Jam_end, &mc.Hari)
 		if err != nil {
@@ -143,8 +184,7 @@ func getMyCourse(w http.ResponseWriter, r *http.Request){
 
 		Mycourse = append(Mycourse, mc)
 
-		
-	}	
+	}
 	fmt.Println(len(Mycourse))
 	json.NewEncoder(w).Encode(Mycourse)
 }
